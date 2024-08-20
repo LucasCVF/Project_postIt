b65ff7b52e5f39e0a017b86efe2b5c107709f0fd
Lucas Cavalcante Ferreira <lucaszeus759@gmial.com>
2024-08-20 12:11:52 -0300

    Validação e inserção dos dados no bancod e dados

--- app_post_it/controle_perfil_usuario.php
+++ app_post_it/controle_perfil_usuario.php
@@ -1,45 +1,115 @@
 <?php 
+    session_start();
 
-print_r($_POST);
-
-function recuperarDadosPerfil() {
-    $dados = ['usuario' => [], 'erro' => []];
-    
-    // Verifica se os dados foram enviados via POST
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        //verifica se existe algo no array POST
-        if (isset($_POST["nome"]) && $_POST["nome"] !== '') {
-            $dados['perfil']['nome'] = $_POST["nome"];
-        } else {
-            $dados['erro'][] = "Nome não informado";
-        } 
-        if (isset($_POST['email']) && $_POST['email'] !== '') {
-            $dados['perfil']['email'] = $_POST["email"];
-        } else {
-            $dados['erro'][] = "Email válido não informado";
-        }
-        if (isset($_POST['estado']) && $_POST['estado'] !== '') {
-            $dados['perfil']['estado'] = $_POST["estado"];
-        } else {
-            $dados['erro'][] = "Estado válido não informado";
-        }
-        if (isset($_POST['cidade']) && $_POST['cidade'] !== '') {
-            $dados['perfil']['cidade'] = $_POST["cidade"];
-        } else {
-            $dados['erro'][] = "Cidade válido não informado";
-        }
-        if (isset($_POST['cep']) && $_POST['cep'] !== '') {
-            $dados['perfil']['cep'] = $_POST["cep"];
-        } else {
-            $dados['erro'][] = "CEP válido não informado";
-        }
+    function recuperarDadosPerfil() {
+        $dados = ['perfil' => [], 'erro' => []];
         
-    }  
-    return $dados;
-}
-echo "<pre>";
-print_r(recuperarDadosPerfil());
-echo "<pre>";
+        // Verifica se os dados foram enviados via POST
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+            // Verifica a quantidade de palavras digitadas no input
+            if (isset($_POST["nome"]) && $_POST["nome"] !== '') {
+                if (str_word_count($_POST["nome"]) >= 2) {
+                    $dados['perfil']['nome'] = $_POST["nome"];
+                } else {
+                    $dados['erro'][] = "Digite seu nome completo";
+                }
+            } else {
+                $dados['erro'][] = "Nome não informado";
+            } 
+
+            // Validação do email cadastrado baseado nos caracteres
+            if (isset($_POST['email']) && $_POST['email'] !== '') {
+                if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    $dados['perfil']['email'] = $_POST["email"];
+                } else {
+                    $dados['erro'][] = "Digite o Email corretamente";
+                }
+            } else {
+                $dados['erro'][] = "Email não informado";
+            }
+
+            if (isset($_POST['estado']) && $_POST['estado'] !== '') {
+                $dados['perfil']['estado'] = $_POST["estado"];
+            } else {
+                $dados['erro'][] = "Estado não informado";
+            }
+            if (isset($_POST['cidade']) && $_POST['cidade'] !== '') {
+                $dados['perfil']['cidade'] = $_POST["cidade"];
+            } else {
+                $dados['erro'][] = "Cidade não informado";
+            }
+
+            if (isset($_POST['cep']) && $_POST['cep'] !== '') {
+                $cep = preg_replace('/\D/', '', $_POST['cep']);
+
+                if (strlen($cep) === 8) {
+                    $dados['perfil']['cep'] = $cep;
+                } else {
+                    $dados['erro'][] = "CEP incorreto, o cep é composto por 8 dígitos";
+                }
+            } else {
+                $dados['erro'][] = "CEP válido não informado";
+            }
+            
+        }  
+        return $dados;
+    }
+    $dadosPerfil = recuperarDadosPerfil();
+
+    // Define a sessão de erro, se houver
+    if (!empty($dadosPerfil['erro'])) {
+        $_SESSION['erro_perfil'] = $dadosPerfil['erro'];
+        $_SESSION['dados_perfil'] = $dadosPerfil['perfil'];
+    } else { 
+        $_SESSION['concluido_perfil'] = $dadosPerfil['perfil'];
+    }
+
+    function inserirDadosPerfil() {
+        $dsn = 'mysql:host=localhost;dbname=db_post_it';
+        $usuario = 'root';
+        $senha = '';
+
+        try {
+            $nome = $_SESSION['concluido_perfil']['nome'];
+            $email = $_SESSION['concluido_perfil']['email'];
+            $estado = $_SESSION['concluido_perfil']['estado'];
+            $cidade = $_SESSION['concluido_perfil']['cidade'];
+            $cep = $_SESSION['concluido_perfil']['cep'];
+            $id_usuario = $_SESSION['id'];
+
+            $conexao = new PDO($dsn, $usuario, $senha);
+            $conexao->beginTransaction();
+
+            $query = "
+                INSERT INTO 
+                `tb_perfil_usuarios`(`id_usuario`, `nome`, `email`, `estado`, `cidade`, `cep`)
+                VALUES (:id_usuario, :nome, :email, :estado, :cidade, :cep);
+            ";
+
+            $stmt = $conexao->prepare($query);
+            $stmt->bindValue(':id_usuario', $id_usuario);
+            $stmt->bindValue(':nome', $nome);
+            $stmt->bindValue(':email', $email);
+            $stmt->bindValue(':estado', $estado);
+            $stmt->bindValue(':cidade', $cidade);
+            $stmt->bindValue(':cep', $cep);
+            $stmt->execute();
+
+            // Confirma a transação
+            $conexao->commit();
+
+        } catch (PDOException $e) {
+            // Reverte a transação em caso de erro
+            $conexao->rollBack();
+            echo 'Erro:' . $e->getCode() . '<br>';
+            echo 'Mensagem:' . $e->getMessage();
+        } catch (Exception $e) {
+            // Trata outros tipos de exceções
+            echo 'Erro:' . $e->getMessage();
+        }
+    }
+    inserirDadosPerfil();
+    header("Location: perfil_usuario.php");
+    exit;
 ?>
--- app_post_it_public/perfil_usuario.php
+++ app_post_it_public/perfil_usuario.php
@@ -16,11 +16,11 @@ require_once "header_usuario.php";
                                     </div>
                                     <div class="col-md-4">
                                         <label for="inputAddress" class="form-label">Nome</label>
-                                        <input type="text" class="form-control bg-input" name="nome" id="inputAddress" placeholder="Informe seu nome completo">
+                                        <input type="text" class="form-control bg-input" name="nome" id="inputAddress" placeholder="Informe seu nome completo" value="<?php echo isset($_SESSION['dados_perfil']['nome']) ? htmlspecialchars($_SESSION['dados_perfil']['nome'])  : ''; ?>">
                                     </div>
                                     <div class="col-md-5">
                                         <label for="inputAddress" class="form-label">Email</label>
-                                        <input type="text" class="form-control bg-input" name="email" id="inputAddress" placeholder="Onde deseja ser notificado de suas atividades">
+                                        <input type="email" class="form-control bg-input" name="email" id="inputAddress" placeholder="Onde deseja ser notificado de suas atividades" value="<?php echo isset($_SESSION['dados_perfil']['email']) ? htmlspecialchars($_SESSION['dados_perfil']['email'])  : ''; ?>">
                                     </div>
                                 </div>
                                 <div class="row mt-5">
@@ -40,12 +40,37 @@ require_once "header_usuario.php";
                                     </div>
                                     <div class="col-md-4">
                                         <label for="inputZip" class="form-label">CEP</label>
-                                        <input type="text" class="form-control bg-input" name="cep" id="inputZip" placeholder="xxxxxxxx">
+                                        <input type="text" pattern="\d{8}" maxlength="8" class="form-control bg-input" name="cep" id="inputZip" placeholder="Digite o cep, 8 números" value="<?php echo isset($_SESSION['dados_perfil']['cep']) ? htmlspecialchars($_SESSION['dados_perfil']['cep'])  : ''; ?>">
                                     </div>
                                 </div>
-
                                 <button type="submit" class="btn bg-button mt-5 w-100">Registrar</button>
-                            </form>
+
+                                <?php
+                                    // Verifica se existe algum erro na sessão
+                                    if (isset($_SESSION['erro_perfil']) && !empty($_SESSION['erro_perfil'])) {
+                                        echo "<h3 class='text-danger mt-3'>Erro no registro </h3>";
+                                        echo "<ul class='text-danger'>";
+                                        foreach ($_SESSION['erro_perfil'] as $mensagem) {
+                                            echo "<li>$mensagem</li>";
+                                            
+                                        }
+                                        echo "</ul>";
+                                        // Limpa os erros da sessão
+                                        unset($_SESSION['erro_perfil']);
+                                        
+                                        
+
+                                    } else if (isset($_SESSION['concluido_perfil']) && !empty($_SESSION['concluido_perfil'])) {
+                                        echo "<div class='text-center'>";
+                                        echo "<h3 class='text-success mt-3'> Registro concluido com sucesso!</h3>";
+                                        echo "<a href='index_usuario.php'><button type='button' class='btn btn-success'> Comece sua atividades </button></a>";
+                                        echo "</div>";
+                                        // Limpa os erros da sessão
+                                        unset($_SESSION['concluido_perfil']);
+                                        unset($_SESSION['dados_perfil']);
+                                    } 
+                                ?>
+                        </form>
                     </div>
                 </div>
             </div>
